d2fde49c85a0f2f80dc5546c103c57f422663647
ajengrhhh <147266996+ajengrhhh@users.noreply.github.com>
2025-03-24 20:31:53 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import express from "express";
 import cors from "cors";
-import UserRoute from "./routes/NoteRoute.js";
+import NoteRoute from "./routes/NoteRoute.js";
 import db from "./config/Database.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(UserRoute);
+app.use(NoteRoute);
 
 // Cek koneksi database saat server start
 async function startServer() {
